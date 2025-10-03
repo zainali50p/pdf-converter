04aa01a52daba922a46e94c83a970817061639cc
zainali50p <zainali50p@gmail.com>
2025-10-02 23:03:46 -0700

    Removed venv and added .gitignore

--- venv/Lib/site-packages/pymupdf/_build.py
+++
@@ -1,7 +0,0 @@
-mupdf_location = 'https://mupdf.com/downloads/archive/mupdf-1.26.7-source.tar.gz'
-pymupdf_version = '1.26.4'
-pymupdf_git_sha = ''
-pymupdf_git_diff = ''
-pymupdf_git_branch = 'main'
-swig_version = '4.3.1'
-swig_version_tuple = (4, 3, 1)
